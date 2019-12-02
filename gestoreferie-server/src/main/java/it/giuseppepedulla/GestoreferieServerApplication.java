08e2f65ab40b19e0a3b9fd3ecbc6eb6075d67e03
VincenzoMattarella <vinsmattarella@gmail.com>
2019-12-02 20:17:15 +0100

    Init
    Init

---
+++ gestoreferie-server/src/main/java/it/giuseppepedulla/GestoreferieServerApplication.java
@@ -0,0 +1,13 @@
+package it.giuseppepedulla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreferieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoreferieServerApplication.class, args);
+	}
+
+}
---
+++ gestoreferie-server/src/test/java/it/giuseppepedulla/GestoreferieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.giuseppepedulla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreferieServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
